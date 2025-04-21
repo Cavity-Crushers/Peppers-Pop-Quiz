73fe86535710a5c65a721d2a4be67da6014108f8
T0xen <jl.graham5002@gmail.com>
2025-04-21 13:34:40 -0400

    Added a categories screen that reads in all the unique categories in the questions.json file as well as an all categories and random category file

---
+++ TriviaGame/Scripts/categories.js
@@ -0,0 +1,59 @@
+// categories.js
+const questionURL = './Data/questions.json';
+
+async function loadCategories() {
+    try {
+        // 1. Fetch question JSON
+        const qResponse = await fetch(questionURL);
+        const qData = await qResponse.json();
+
+        // These 2 lines and the for-loop are based on: https://stackoverflow.com/questions/9895082/javascript-populate-drop-down-list-with-array
+        var select = document.getElementById("selectCategory");
+        var categories = [];
+
+        for (var i = 0; i < qData.question.length; i++) {
+            var category = qData.question[i].category;
+            if (!categories.includes(category)) {
+                categories.push(category);
+                var cat = document.createElement("option");
+                cat.textContent = category;
+                cat.value = category;
+                select.appendChild(cat);
+            }
+        }
+        var allCats = document.createElement("option");
+        allCat.textContent = "All Categories";
+        allCats.value = "All Categories";
+        select.appendChild(allCats);
+
+        var randomCat = document.createElement("option");
+        randomCat.textContent = "Random Category";
+        randomCat.value = "Random Category";
+        select.appendChild(randomCat);
+
+    } catch (err) {
+        console.error('Error loading questions:', err);
+    }
+}
+
+/**
+ * Takes the player back to the game page
+ */
+async function goToGame() {
+    window.location.href = './game.html';
+}
+
+/**
+ * Takes the player back to the home page and clears their progress from that run
+ */
+async function goToHome() {
+    window.location.href = './index.html';
+    localStorage.clear();
+}
+
+/**
+ * Calls all of the functions that do not run on answer selection to initialize all variables
+ */
+window.addEventListener('DOMContentLoaded', () => {
+    loadCategories();
+});
